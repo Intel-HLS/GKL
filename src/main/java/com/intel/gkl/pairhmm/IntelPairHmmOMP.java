baf235fe3412fa4c73925c012b080cddc198533b
pnvaidya <priya.n.vaidya@intel.com>
2016-11-03 12:16:51 -0400

    added dynamic huffman and PairHMM edits

---
+++ src/main/java/com/intel/gkl/pairhmm/IntelPairHmmOMP.java
@@ -0,0 +1,12 @@
+package com.intel.gkl.pairhmm;
+
+/**
+ * Created by pnvaidya on 11/2/16.
+ */
+public class IntelPairHmmOMP extends IntelPairHmm  {
+
+    static {
+        setLibFileName("gkl_pairhmm_omp");
+        setIsLoaded(false);
+    }
+}
