aa02f124b99f0d7ad300d3beaade221ea90a4baf
Srikanth Yalavarthi <syalavarthi@marvell.com>
2020-01-05 02:45:45 +0000

    updated datatypes in neon-types.h
        - __m128 -> float32x4_t
        - __m128d -> float64x2_t
        - __m128i -> int32x4_t
        - __m64   -> float32x2_t
        - datatypes __m512, __m512d, __m512i, __m256, __m256d, __m256i are commented

--- src/main/native/pairhmm/neon-types.h
+++ src/main/native/pairhmm/neon-types.h
@@ -1,9 +1,9 @@
-#ifndef AVX_TEMPLATE_H
-#define AVX_TEMPLATE_H
-
+#ifndef NEON_TEMPLATE_H
+#define NEON_TEMPLATE_H
 
 #define ALIGNED __attribute__((aligned(32)))
 
+/*
 typedef union __attribute__((aligned(32))) {
         ALIGNED __m512 ALIGNED d;
         ALIGNED __m256i ALIGNED s[2];
@@ -13,50 +13,55 @@ typedef union __attribute__((aligned(32))) {
 
 typedef union __attribute__((aligned(32))) {
         ALIGNED __m256 ALIGNED d;
-        ALIGNED __m128i ALIGNED s[2];
+        ALIGNED int32x4_t ALIGNED s[2];
         ALIGNED float  ALIGNED f[8];
         ALIGNED __m256i ALIGNED i;
 } ALIGNED mix_F256 ALIGNED;
+*/
 
 typedef union __attribute__((aligned(32))) {
-        ALIGNED __m128 ALIGNED d;
-        ALIGNED __m64 ALIGNED s[2];
+        ALIGNED float32x4_t ALIGNED d;
+        ALIGNED float32x2_t ALIGNED s[2];
         ALIGNED float  ALIGNED f[4];
-        ALIGNED __m128i ALIGNED i;
+        ALIGNED int32x4_t ALIGNED i;
 } ALIGNED mix_F128 ALIGNED;
 
 /* -------------------------------------------------------------------------- */
 
+/*
 typedef union ALIGNED {
   __m256i vec;
   __m256 vecf;
   uint32_t masks[8];
 } MaskVec_F512;
+*/
 
 typedef union ALIGNED {
-  __m128i vec;
-  __m128 vecf;
+  int32x4_t vec;
+  float32x4_t vecf;
   uint32_t masks[4];
 } MaskVec_F256;
 
 typedef union ALIGNED {
-  __m64 vec;
-  __m64 vecf;
+  float32x2_t vec;
+  float32x2_t vecf;
   uint32_t masks[2];
 } MaskVec_F128;
 
 /* -------------------------------------------------------------------------- */
 
+/*
 typedef union ALIGNED
 {
         ALIGNED __m256i ALIGNED i;
         ALIGNED __m256 ALIGNED f;
 } ALIGNED IF_256f ALIGNED;
+*/
 
 typedef union ALIGNED
 {
-        ALIGNED __m128i ALIGNED i;
-        ALIGNED __m128 ALIGNED f;
+        ALIGNED int32x4_t ALIGNED i;
+        ALIGNED float32x4_t ALIGNED f;
 } ALIGNED IF_128f ALIGNED;
 
 typedef union ALIGNED
@@ -67,6 +72,7 @@ typedef union ALIGNED
 
 /* -------------------------------------------------------------------------- */
 
+/*
 typedef union __attribute__((aligned(32))) {
         ALIGNED __m512d ALIGNED d;
         ALIGNED __m256 ALIGNED s[2];
@@ -76,50 +82,55 @@ typedef union __attribute__((aligned(32))) {
 
 typedef union __attribute__((aligned(32))) {
         ALIGNED __m256d ALIGNED d;
-        ALIGNED __m128i ALIGNED s[2];
+        ALIGNED int32x4_t ALIGNED s[2];
         ALIGNED double  ALIGNED f[4];
         ALIGNED __m256i ALIGNED i;
 } ALIGNED mix_D256 ALIGNED;
+*/
 
 typedef union __attribute__((aligned(32))) {
-        ALIGNED __m128d ALIGNED d;
-        ALIGNED __m64 ALIGNED s[2];
+        ALIGNED float64x2_t ALIGNED d;
+        ALIGNED float32x2_t ALIGNED s[2];
         ALIGNED double  ALIGNED f[2];
-        ALIGNED __m128i ALIGNED i;
+        ALIGNED int32x4_t ALIGNED i;
 } ALIGNED mix_D128 ALIGNED;
 
 /* -------------------------------------------------------------------------- */
 
+/*
 typedef union ALIGNED {
   __m256i vec;
   __m256d vecf;
   uint64_t masks[4];
 } MaskVec_D512;
+*/
 
 typedef union ALIGNED {
-  __m128i vec;
-  __m128d vecf;
+  int32x4_t vec;
+  float64x2_t vecf;
   uint64_t masks[2];
 } MaskVec_D256;
 
 typedef union ALIGNED {
-  __m64 vec;
-  __m64 vecf;
+  float32x2_t vec;
+  float32x2_t vecf;
   uint64_t masks[1];
 } MaskVec_D128;
 
 /* -------------------------------------------------------------------------- */
 
+/*
 typedef union ALIGNED
 {
         ALIGNED __m256i ALIGNED i;
         ALIGNED __m256d ALIGNED f;
 } ALIGNED IF_256d ALIGNED;
+*/
 
 typedef union ALIGNED
 {
-        ALIGNED __m128i ALIGNED i;
-        ALIGNED __m128d ALIGNED f;
+        ALIGNED int32x4_t ALIGNED i;
+        ALIGNED float64x2_t ALIGNED f;
 } ALIGNED IF_128d ALIGNED;
 
 typedef union ALIGNED
@@ -128,4 +139,4 @@ typedef union ALIGNED
         ALIGNED double  ALIGNED f;
 } ALIGNED IF_64 ALIGNED;
 
-#endif // AVX_TEMPLATE_H
+#endif // NEON_TEMPLATE_H
