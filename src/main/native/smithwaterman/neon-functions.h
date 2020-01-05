01f059d1396b17a37274be097a573c627cf7bde4
Srikanth Yalavarthi <syalavarthi@marvell.com>
2020-01-05 17:49:51 +0000

    EOD commit, updated neon intrinsics
      * Pending conversions, 3
        - VEC_BLEND
        - VEC_PERMUTE2x128_EVEN
        - VEC_PERMUTE2x128_ODD

--- src/main/native/smithwaterman/neon-functions.h
+++ src/main/native/smithwaterman/neon-functions.h
@@ -1,7 +1,7 @@
 #ifndef _NEON_FUNCTIONS_H
 #define _NEON_FUNCTIONS_H
 
-#undef AVX_LENGTH
+#undef NEON_LENGTH
 #undef VEC_INT_TYPE
 #undef VEC_LOADU
 #undef VEC_STORE
@@ -20,60 +20,73 @@
 #undef VEC_PERMUTE2x128
 #undef VEC_PACKS_32
 
-#define AVX_LENGTH 8
+#define NEON_LENGTH 4
 
-#define VEC_INT_TYPE __m256i
-#define VEC_MASK_TYPE __m256i
+#define VEC_INT_TYPE int32x4_t
+#define VEC_MASK_TYPE int32x4_t
 
 #define VEC_LOADU(__addr) \
-    _mm256_loadu_si256((__m256i *)(__addr))
+    vld1q_s32((const int32_t *) __addr)
+//  _mm256_loadu_si256((__m256i *)(__addr))
 
 #define VEC_STORE(__addr, __v) \
-    _mm256_store_si256((__m256i *)(__addr), __v)
+    vst1q_s32((int32_t *) __addr, __v)
+//  _mm256_store_si256((__m256i *)(__addr), __v)
 
 #define VEC_STOREU(__addr, __v) \
-    _mm256_storeu_si256((__m256i *)(__addr), __v)
+    vst1q_s32((int32_t *) __addr, __v)
+//  _mm256_storeu_si256((__m256i *)(__addr), __v)
 
 #define VEC_STREAM(__addr, __v) \
-    _mm256_stream_si256((__m256i *)(__addr), __v)
-
+    vst1q_s32((int32_t *) __addr, __v)
+//  _mm256_stream_si256((__m256i *)(__addr), __v)
 
 #define VEC_SET_ZERO() \
-    _mm256_setzero_si256()
+    vdupq_n_s32(0)
+//  _mm256_setzero_si256()
 
 #define VEC_SET1_VAL32(__val) \
-    _mm256_set1_epi32(__val)
+    vdupq_n_s32(__val)
+//  _mm256_set1_epi32(__val)
 
 #define VEC_ADD(__v1, __v2) \
-    _mm256_add_epi32(__v1, __v2)
+    vaddq_s32(__v1, __v2)
+//  _mm256_add_epi32(__v1, __v2)
 
 #define VEC_MAX(__v1, __v2) \
-    _mm256_max_epi32(__v1, __v2)
+    vmaxq_s32(__v1, __v2)
+//  _mm256_max_epi32(__v1, __v2)
     
 #define VEC_CMPGT(__v1, __v2) \
-    _mm256_cmpgt_epi32(__v1, __v2)
+    vreinterpretq_s32_u32(vcgtq_s32(__v1, __v2))
+//  _mm256_cmpgt_epi32(__v1, __v2)
 
 #define VEC_CMPGT_MASK(__v1, __v2) \
-    _mm256_cmpgt_epi32(__v1, __v2)
+    vreinterpretq_s32_u32(vcgtq_s32(__v1, __v2))
+//  _mm256_cmpgt_epi32(__v1, __v2)
 
 #define VEC_CMPEQ(__v1, __v2) \
-    _mm256_cmpeq_epi32(__v1, __v2)
+    vreinterpretq_s32_u32(vceqq_s32(__v1, __v2))
+//  _mm256_cmpeq_epi32(__v1, __v2)
 
 #define VEC_CMPEQ_MASK(__v1, __v2) \
-    _mm256_cmpeq_epi32(__v1, __v2)
+    vreinterpretq_s32_u32(vceqq_s32(__v1, __v2))
+//  _mm256_cmpeq_epi32(__v1, __v2)
 
 #define VEC_AND(__v1, __v2) \
-    _mm256_and_si256(__v1, __v2)
+    vandq_s32(__v1, __v2)
+//  _mm256_and_si256(__v1, __v2)
 
 #define VEC_OR(__v1, __v2) \
-    _mm256_or_si256(__v1, __v2)
+    vorrq_s32(__v1, __v2)
+//  _mm256_or_si256(__v1, __v2)
 
 #define VEC_ANDNOT(__v1, __v2) \
-    _mm256_andnot_si256(__v1, __v2)
+    vbicq_s32(__v2, __v1)
+//  _mm256_andnot_si256(__v1, __v2)
 
 #define VEC_BLEND(__v1, __v2, __mask) \
     (__m256i)_mm256_blendv_ps((__m256)__v1, (__m256)__v2, (__m256)__mask)
-
 
 #define VEC_PERMUTE2x128_EVEN(__v1, __v2) \
     _mm256_permute2f128_si256(__v1, __v2, 0x20)    
@@ -82,7 +95,8 @@
     _mm256_permute2f128_si256(__v1, __v2, 0x31)    
 
 #define VEC_PACKS_32(__v1, __v2) \
-    _mm256_packs_epi32(__v1, __v2)
+    vreinterpretq_s32_s16(vcombine_s16(vqmovn_s32(__v1), vqmovn_s32(__v2)))
+//  _mm256_packs_epi32(__v1, __v2)
 
 #define INIT_CONSTANTS 
 
