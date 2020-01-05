db4deb3341229a145151a973e306a33c6e890b8c
Srikanth Yalavarthi <syalavarthi@marvell.com>
2020-01-05 05:40:17 +0000

    initial changes to neon-functions-double.h

--- src/main/native/pairhmm/neon-functions-double.h
+++ src/main/native/pairhmm/neon-functions-double.h
@@ -10,8 +10,7 @@
 #undef SHIFT_CONST3
 #undef _128_TYPE
 #undef SIMD_TYPE
-#undef _256_INT_TYPE
-#undef AVX_LENGTH
+#undef NEON_LENGTH
 #undef HAP_TYPE
 #undef MASK_TYPE
 #undef MASK_ALL_ONES
@@ -32,6 +31,7 @@
 #undef VEC_MOVE
 #undef VEC_CAST_128_256
 #undef VEC_INSERT_VAL
+#undef VEC_INSERT_UNIT
 #undef VEC_CVT_128_256
 #undef VEC_SET1_VAL
 #undef VEC_POPCVT_CHAR
@@ -41,6 +41,7 @@
 #undef SHIFT_HAP
 #undef VEC_SSE_TO_AVX
 #undef VEC_SHIFT_LEFT_1BIT
+#undef VEC_128_SHIFT_LEFT
 #undef COMPARE_VECS
 #undef BITMASK_VEC
 #endif
@@ -48,44 +49,44 @@
 #define PRECISION d
 #define MAIN_TYPE double
 #define MAIN_TYPE_SIZE 64
-#define UNION_TYPE mix_D256
+#define UNION_TYPE mix_D128
 #define IF_128 IF_128d
 #define IF_MAIN_TYPE IF_64
-#define SHIFT_CONST1 8
-#define SHIFT_CONST2 1
-#define SHIFT_CONST3 8
-#define _128_TYPE __m128d
-#define SIMD_TYPE __m256d
-#define _256_INT_TYPE __m256i
-#define AVX_LENGTH 4
-#define HAP_TYPE __m128i
+#define SHIFT_CONST1 1
+#define SHIFT_CONST2 8
+#define SHIFT_CONST3 0
+#define _128_TYPE float64x2_t
+#define SIMD_TYPE float64x2_t
+#define NEON_LENGTH 4
+#define HAP_TYPE UNION_TYPE
 #define MASK_TYPE uint64_t
-#define MASK_ALL_ONES 0xFFFFFFFFFFFFFFFF
-#define MASK_VEC MaskVec_D256
+#define MASK_ALL_ONES 0xFFFFFFFF
+#define MASK_VEC MaskVec_D128
 
 #define SET_VEC_ZERO(__vec)                     \
     __vec= _mm256_setzero_pd()
 
 #define VEC_OR(__v1, __v2)                      \
-    _mm256_or_pd(__v1, __v2)
+    vorrq_s64(__v1, __v2)
 
 #define VEC_ADD(__v1, __v2)                     \
-    _mm256_add_pd(__v1, __v2)
+    vaddq_f64(__v1, __v2)
 
 #define VEC_SUB(__v1, __v2)                     \
-    _mm256_sub_pd(__v1, __v2)
+    vsubq_f64(__v1, __v2)
 
 #define VEC_MUL(__v1, __v2)                     \
-    _mm256_mul_pd(__v1, __v2)
+    vmulq_f64(__v1, __v2)
 
 #define VEC_DIV(__v1, __v2)                     \
-    _mm256_div_pd(__v1, __v2)
+    vdivq_f64(__v1, __v2)
 
 #define VEC_BLEND(__v1, __v2, __mask)           \
     _mm256_blend_pd(__v1, __v2, __mask)
 
-#define VEC_BLENDV(__distmChosen, __v1, __v2, __maskV)      \
-    __distmChosen = _mm256_blendv_pd(__v1, __v2, __maskV);
+#define VEC_BLENDV(__v1, __v2, __maskV)      \
+    vreinterpretq_f64_s64(vbslq_s64(vcgeq_u64(vreinterpretq_u64_f64(__maskV), vdupq_n_u64(0x80000000)),  \
+       vreinterpretq_s64_f64(__v2), vreinterpretq_s64_f64(__v1)))
 
 #define VEC_CAST_256_128(__v1)                  \
     _mm256_castpd256_pd128 (__v1)
@@ -94,7 +95,7 @@
     _mm256_extractf128_pd (__v1, __im)
 
 #define VEC_EXTRACT_UNIT(__v1, __im)            \
-    _mm_extract_epi64(__v1, __im)
+    vgetq_lane_s64(__v1, __im)
 
 #define VEC_SET1_VAL128(__val)                  \
     _mm_set1_pd(__val)
@@ -108,11 +109,14 @@
 #define VEC_INSERT_VAL(__v1, __val, __pos)      \
     _mm256_insertf128_pd(__v1, __val, __pos)
 
+#define VEC_INSERT_UNIT(__v1, __v2, __im)       \
+    vsetq_lane_s64(__v2, __v1, __im)
+
 #define VEC_CVT_128_256(__v1)                   \
     _mm256_cvtepi32_pd(__v1)
 
 #define VEC_SET1_VAL(__val)                     \
-    _mm256_set1_pd(__val)
+    vdupq_n_f64(__val)
 
 #define VEC_POPCVT_CHAR(__ch)                   \
     _mm256_cvtepi32_pd(_mm_set1_epi32(__ch))
@@ -134,8 +138,10 @@
 __vdst = _mm256_insertf128_pd(__vdst, __vsHigh, 1) ;
 
 #define VEC_SHIFT_LEFT_1BIT(__vs)               \
-    __vs = _mm_slli_epi64(__vs, 1)
+    __vs = vshlq_n_s64(__vs, 1)
 
+#define VEC_128_SHIFT_LEFT(__vs, __im)          \
+    vreinterpretq_s64_s8(vextq_s8(vdupq_n_s8(0), vreinterpretq_s8_s64(__vs), 16 - (__im)))
 
 #define COMPARE_VECS(__v1, __v2, __first, __last) {                     \
     double* ptr1 = (double*) (&__v1) ;                                  \
@@ -151,25 +157,24 @@ __vdst = _mm256_insertf128_pd(__vdst, __vsHigh, 1) ;
 
 class BitMaskVec_double {
 
-    MASK_VEC low_, high_ ;
-    SIMD_TYPE combined_ ;
+    MASK_VEC combined_ ;
 
     public:
+
     inline MASK_TYPE& getLowEntry(int index) {
-        return low_.masks[index] ;
+        return combined_.masks[index] ;
     }
+
     inline MASK_TYPE& getHighEntry(int index) {
-        return high_.masks[index] ;
+        return combined_.masks[NEON_LENGTH/2+index] ;
     }
 
     inline const SIMD_TYPE& getCombinedMask() {
-        VEC_SSE_TO_AVX(low_.vecf, high_.vecf, combined_) ;
-        return combined_ ;
+        return combined_.vecf ;
     }
 
     inline void shift_left_1bit() {
-        VEC_SHIFT_LEFT_1BIT(low_.vec) ;
-        VEC_SHIFT_LEFT_1BIT(high_.vec) ;
+        VEC_SHIFT_LEFT_1BIT(combined_.vec) ;
     }
 
 } ;
