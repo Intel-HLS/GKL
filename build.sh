72259561e5ff611cc72eff3ea537f1a775c0c9ee
Snehal Adsule <19539903+SnehalA@users.noreply.github.com>
2020-09-29 15:13:25 -0700

    resolve conflict

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
  install_dep(){
-<<<<<<< HEAD
      if ! rpm -qa | grep git-lfs; then
         curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
         sudo yum install -y git-lfs
@@ -20,16 +19,6 @@ echo "Checking dependencies"
 install_dep
 
 echo "Setting GCC"
-=======
- curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
- sudo yum install -y git-lfs
- git lfs install
- sudo yum install -y java-1.8.0-openjdk-devel git cmake patch libtool automake yasm zlib-devel centos-release-scl
- sudo yum install -y devtoolset-7-gcc-*
-}
-echo "Installing dependencies"
-#install_dep
->>>>>>> 16332429590ea845bcbbb749ff405394a6aed4ce
 source scl_source enable devtoolset-7
 gcc --version
 
