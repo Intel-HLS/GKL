2c0af7f618d1cec0c59a7c351076ce2f528e0d59
Michał Jakubowicz <michal.piotr.jakubowicz@intel.com>
2021-01-26 15:45:26 +0100

    Fix finish condition
    ISAL_END_INPUT is not docummented as an output of isal_inflate (it's
    only used in isal_inflate_stateless). With the error condition changed
    the Java object will now properly reflect the state of the compression
    stream as specified in the Inflater class.

--- src/main/native/compression/IntelInflater.cc
+++ src/main/native/compression/IntelInflater.cc
@@ -174,13 +174,7 @@ JNIEXPORT jint JNICALL Java_com_intel_gkl_compression_IntelInflater_inflateNativ
         env->ReleasePrimitiveArrayCritical(inputBuffer, next_in, 0);
         env->ReleasePrimitiveArrayCritical(outputBuffer, next_out, 0);
 
-        /*
-        As far as I can tell this block never gets triggered as ISAL_END_INPUT
-        isn't listed as a possible return of isal_inflate(). I don't have a good
-        understanding of how this value is being used by the Java class but this
-        should either be fixed or removed.
-        */
-        if (ret == ISAL_END_INPUT && lz_stream->avail_in == 0) {
+        if (ret == ISAL_DECOMP_OK && lz_stream->avail_in == 0) {
           env->SetBooleanField(obj, FID_inf_finished, true);
         }
 
